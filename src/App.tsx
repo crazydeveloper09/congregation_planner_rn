6b9c66420c12555e0d1cfc9bf99a9dbdff7795b3
crazydeveloper09 <maciejkuta6@gmail.com>
2025-05-24 00:40:37 +0200

    try to fix build issue v4

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,37 @@
 import { NavigationContainer } from '@react-navigation/native';
-import { Provider as AuthProvider } from './src/contexts/AuthContext';
-import { Provider as MeetingProvider } from './src/contexts/MeetingContext';
-import { Provider as MinistryMeetingProvider } from './src/contexts/MinistryMeetingContext';
-import { Provider as CartsScheduleProvider } from './src/contexts/CartsScheduleContext';
-import { Provider as AudioVideoProvider } from './src/contexts/AudioVideoContext';
-import { Provider as OrdinalsProvider } from './src/contexts/AttendantsContext';
-import { Provider as PreachersProvider } from './src/contexts/PreachersContext';
-import { Provider as MinistryGroupProvider } from './src/contexts/MinistryGroupContext';
-import { Provider as SettingsProvider } from './src/contexts/SettingsContext';
+import { Provider as AuthProvider } from './contexts/AuthContext';
+import { Provider as MeetingProvider } from './contexts/MeetingContext';
+import { Provider as MinistryMeetingProvider } from './contexts/MinistryMeetingContext';
+import { Provider as CartsScheduleProvider } from './contexts/CartsScheduleContext';
+import { Provider as AudioVideoProvider } from './contexts/AudioVideoContext';
+import { Provider as OrdinalsProvider } from './contexts/AttendantsContext';
+import { Provider as PreachersProvider } from './contexts/PreachersContext';
+import { Provider as MinistryGroupProvider } from './contexts/MinistryGroupContext';
+import { Provider as SettingsProvider } from './contexts/SettingsContext';
 import { Alert, StatusBar } from 'react-native';
 import { useFonts } from 'expo-font';
-import { navigationRef } from './src/RootNavigation';
-import SwitchNavigator from './src/navigators/Switch';
+import { navigationRef } from './RootNavigation';
+import SwitchNavigator from './navigators/Switch';
 import FlashMessage from 'react-native-flash-message';
 import * as Calendar from 'expo-calendar';
 import { useContext, useEffect, useState } from 'react';
-import PreachersNavigator from './src/navigators/Preachers';
+import PreachersNavigator from './navigators/Preachers';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 StatusBar.setBarStyle('light-content')
 
 function App() {
   
   const [fontsLoaded] = useFonts({
-    'FontAwesome': require('./assets/fonts/FontAwesome.ttf'),
-    'InterThin': require('./assets/fonts/inter/Inter-Thin.ttf'),
-    'InterRegular': require('./assets/fonts/inter/Inter-Regular.ttf'),
-    'InterSemiBold': require('./assets/fonts/inter/Inter-SemiBold.ttf'),
-    'MontserratRegular': require('./assets/fonts/montserrat/Montserrat-Regular.ttf'),
-    'MontserratSemiBold': require('./assets/fonts/montserrat/Montserrat-SemiBold.ttf'),
-    'PoppinsThin': require('./assets/fonts/Poppins/Poppins-Thin.ttf'),
-    'PoppinsSemiBold': require('./assets/fonts/Poppins/Poppins-SemiBold.ttf'),
-    'PoppinsRegular': require('./assets/fonts/Poppins/Poppins-Regular.ttf'),
-    'SatisfyRegular': require('./assets/fonts/Satisfy/Satisfy-Regular.ttf')
+    'FontAwesome': require('../assets/fonts/FontAwesome.ttf'),
+    'InterThin': require('../assets/fonts/inter/Inter-Thin.ttf'),
+    'InterRegular': require('../assets/fonts/inter/Inter-Regular.ttf'),
+    'InterSemiBold': require('../assets/fonts/inter/Inter-SemiBold.ttf'),
+    'MontserratRegular': require('../assets/fonts/montserrat/Montserrat-Regular.ttf'),
+    'MontserratSemiBold': require('../assets/fonts/montserrat/Montserrat-SemiBold.ttf'),
+    'PoppinsThin': require('../assets/fonts/Poppins/Poppins-Thin.ttf'),
+    'PoppinsSemiBold': require('../assets/fonts/Poppins/Poppins-SemiBold.ttf'),
+    'PoppinsRegular': require('../assets/fonts/Poppins/Poppins-Regular.ttf'),
+    'SatisfyRegular': require('../assets/fonts/Satisfy/Satisfy-Regular.ttf')
   });
   const [permissionStatus, setPermissionStatus] = useState<string>('')
   const loadCalendarPermission = async () => {
