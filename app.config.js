f4cb6ab90a0fb4ebc3da2afd8bf6e35b96e023b1
crazydeveloper09 <maciejkuta6@gmail.com>
2025-06-03 14:59:30 +0200

    adjust lighten for colors

--- app.config.js
+++ app.config.js
@@ -18,7 +18,7 @@ export default {
     ],
     "ios": {
       "supportsTablet": true,
-      "buildNumber": "17",
+      "buildNumber": "18",
       "bundleIdentifier": "com.miszki.congregation-planner",
       "infoPlist": {
         "NSCalendarsUsageDescription": "Ta aplikacja do przydatnych dla Ciebie funkcji potrzebuje dostępu do kalendarza",
