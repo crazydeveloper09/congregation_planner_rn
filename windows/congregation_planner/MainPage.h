29620491531966004c749fab14514583cbac808d
crazydeveloper09 <maciejkuta6@gmail.com>
2025-06-13 14:48:00 +0200

    not fully working windows exmple

---
+++ windows/congregation_planner/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::congregation_planner::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::congregation_planner::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
