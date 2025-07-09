c6600d8594ff90921f80bc276882ed569dc915ff
crazydeveloper09 <maciejkuta6@gmail.com>
2025-07-09 10:50:57 +0200

    fix apple comments from review

--- app.config.js
+++ app.config.js
@@ -18,7 +18,7 @@ export default {
     ],
     "ios": {
       "supportsTablet": true,
-      "buildNumber": "4",
+      "buildNumber": "5",
       "bundleIdentifier": "com.miszki.congregation-planner",
       "infoPlist": {
         "NSCalendarsUsageDescription": "Ta aplikacja do przydatnych dla Ciebie funkcji potrzebuje dostępu do kalendarza",
