54654b279f612e6333b6b5abdf60af9c9ef9462a
mkutaGD <mkuta@griddynamics.com>
2025-07-15 10:46:16 +0200

    add new info

--- app.config.js
+++ app.config.js
@@ -18,7 +18,7 @@ export default {
     ],
     "ios": {
       "supportsTablet": true,
-      "buildNumber": "5",
+      "buildNumber": "7",
       "bundleIdentifier": "com.miszki.congregation-planner",
       "infoPlist": {
         "NSCalendarsUsageDescription": "Ta aplikacja do przydatnych dla Ciebie funkcji potrzebuje dostępu do kalendarza",
